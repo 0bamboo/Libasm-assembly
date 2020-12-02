b83331604c946b5aa8e057d381c0cf66f88e5bd8
Paayk <itsabdennacerama@gmail.com>
2020-12-02 14:59:32 +0100

    asm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 13:13:50 by abdait-m          #+#    #+#             */
-/*   Updated: 2020/12/02 14:28:33 by abdait-m         ###   ########.fr       */
+/*   Updated: 2020/12/02 14:58:09 by abdait-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@ int     main(int argc, char **argv)
 {
     int a,b;
     int n;
-    int bz = 5;
+    int bz = 1;
     char *s = malloc(10);
+    errno = 0;
     printf("================= WRITE FUNC : ================ \n");
-    ft_write(1,"that's hahahah\n", 16);
-    write(1,"that's hahahah\n", 16);
+    ft_write(1,"that's hahahah\n", -1);
+	printf("| errno | = |%d|\n", errno);
+    errno = 0;
+    write(1,"that's hahahah\n", -1);
+	printf("| errno | = |%d|\n", errno);
     printf("================ READ FUNC : ================= \n");
+    errno = 0;
     int fd1 = open("test1", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
 	int fd2 = open("test1", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
 	n = ft_read(fd1, s, bz);
